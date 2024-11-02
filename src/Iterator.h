1b8f8e068b9ca85457e4b1a2db5b053925f1e30b
David Musa-Aisien <cal24815@gmail.com>
2024-11-03 00:21:03 +0200

    Update Iterator.h

--- src/Iterator.h
+++ src/Iterator.h
@@ -1,12 +1,21 @@
 #ifndef ITERATOR_H
 #define ITERATOR_H
 
+#include <vector>
 #include "RoadNetwork.h"
 
-class Iterator
+class Iterator 
 {
+private:
+    std::vector<Road*> traversedRoads;
+    int size;
 
+public:
+    virtual void first() = 0;
+    virtual void next() = 0;
+    virtual Road* current() const = 0;
+    virtual bool isDone() const = 0;
+    virtual ~Iterator() = default;
 };
-
 
 #endif
