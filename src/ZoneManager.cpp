e433c7d11cdd7886769d49c9af0e5bf170f550cc
Kelsey Hamann <kelseydhamann@gmail.com>
2024-11-02 20:13:35 +0200

    Update ZoneManager.cpp

--- src/ZoneManager.cpp
+++ src/ZoneManager.cpp
@@ -4,13 +4,13 @@
 #include <vector>
 
 Zone* ZoneManager::createZone(std::string type, Point* location){
-    throw "Not Implemented yet."
+    throw "Not Implemented yet.";
 }
 
 bool ZoneManager::checkZoneRequirements(Building* building){
-    throw "Not Implemented yet."
+    throw "Not Implemented yet.";
 }
 
 std::vector<Zone*> ZoneManager::getAvailableZones(){
-    throw "Not Implemented yet."
+    throw "Not Implemented yet.";
 }
