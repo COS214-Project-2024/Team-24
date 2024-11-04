254a6646971a1dd51caea6abd79a207048e9eb2b
David Musa-Aisien <cal24815@gmail.com>
2024-11-04 15:16:40 +0200

    Update Commercial.h

--- src/Commercial.h
+++ src/Commercial.h
@@ -2,10 +2,15 @@
 #define COMMERCIAL_H
 
 #include "Building.h"
+#include "BuildingVisitor.h"
+#include "UnderMaintenance.h"
 
 class Commercial : Building {
 public:
 	void build();
+	void accept(BuildingVisitor* visitor);
+	void maintain();
+	void inspect();
 };
 
 #endif
