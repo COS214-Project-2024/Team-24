1990ba155c31e3385c78b84c718c44543adb664b
owethu-dlamini <u23546702@tuks.co.za>
2024-11-04 05:06:49 +0200

    Create CitizenManager.cpp

---
+++ src/CitizenManager.cpp
@@ -0,0 +1,21 @@
+#include "CitizenManager.h"
+
+Citizen* CitizenManager::createCitizen(std::string type)
+{
+    return NULL;
+}
+
+void CitizenManager::updateCitizens()
+{
+
+}
+
+void CitizenManager::handleCitizenNeeds()
+{
+
+}
+
+double CitizenManager::calculateHappiness()
+{
+    return 0;
+}
