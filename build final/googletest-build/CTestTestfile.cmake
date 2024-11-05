cababbdc3e2462eac32278eb4d342f0dce820151
Kelsey Hamann <kelseydhamann@gmail.com>
2024-11-05 07:57:09 +0200

    Add files via upload

---
+++ build final/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/velen/Music/Computer Science/2024/COS214/project/build/_deps/googletest-src
+# Build directory: /mnt/c/Users/velen/Music/Computer Science/2024/COS214/project/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build final/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/velen/Music/Computer Science/2024/COS214/project/build/_deps/googletest-src/googlemock
+# Build directory: /mnt/c/Users/velen/Music/Computer Science/2024/COS214/project/build/_deps/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
